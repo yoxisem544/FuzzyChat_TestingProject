529c31c1c2e75a0450cf5929a310b3c733be06a5
David <yoxisem544@gmail.com>
2015-12-21 11:51:53 +0800

    add bridging header

---
+++ FuzzyChatTestingProjectHeader.h
@@ -0,0 +1,16 @@
+//
+//  FuzzyChatTestingProjectHeader.h
+//  FuzzyChat_Testing
+//
+//  Created by David on 2015/12/21.
+//  Copyright © 2015年 David. All rights reserved.
+//
+
+#ifndef FuzzyChatTestingProjectHeader_h
+#define FuzzyChatTestingProjectHeader_h
+
+#import "JSQMessages.h"
+#import "JSQMessageData.h"
+#import "Firebase.h"
+
+#endif /* FuzzyChatTestingProjectHeader_h */
