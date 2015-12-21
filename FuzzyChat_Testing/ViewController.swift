24b52e1582e995ab418874d79cb6ddd8d374807b
David <yoxisem544@gmail.com>
2015-12-21 21:21:14 +0800

    try to understand query

--- FuzzyChat_Testing/ViewController.swift
+++ FuzzyChat_Testing/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var myRootRef = Firebase(url: "https://fuzzychat-testing.firebaseio.com/")
+    var myRootRef = Firebase(url: "https://dinosaur-facts.firebaseio.com/dinosaurs")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var temp_users = myRootRef.childByAppendingPath("saving-data/temp_users")
-        var alanisawesome = ["full_name": "Alan Turing", "date_of_birth": "June 23, 1912"]
-        var gracehop = ["full_name": "Grace Hopper", "date_of_birth": "December 9, 1906"]
-        var users = ["alanisawesome": alanisawesome, "gracehop": gracehop]
-        temp_users.setValue(users)
+        let temp_users = myRootRef.childByAppendingPath("saving-data/temp_users")
+        let alanisawesome = ["full_name": "Alan Turing", "date_of_birth": "June 23, 1912"]
+        let gracehop = ["full_name": "Grace Hopper", "date_of_birth": "December 9, 1906"]
+        let users = ["alanisawesome": alanisawesome,"gracehop": gracehop, "alanisawesome1": alanisawesome,"gracehop1": gracehop]
+//        temp_users.setValue(users)
+//        temp_users.updateChildValues(users)
+//        temp_users.setValue(users)
+        temp_users.observeEventType(FEventType.Value) { (snapshot: FDataSnapshot!) -> Void in
+//            print(snapshot)
+        }
+        temp_users.queryEqualToValue(1).observeEventType(.Value) { (snapshot: FDataSnapshot!) -> Void in
+//            print(snapshot)
+        }
+        myRootRef.queryOrderedByChild("weight").observeEventType(.Value) { (sn: FDataSnapshot!) -> Void in
+//            print(sn)
+        }
+        myRootRef.queryOrderedByChild("weight").queryEqualToValue(135000).observeEventType(.Value) { (s: FDataSnapshot!) -> Void in
+//            print(s)
+        }
+        myRootRef.queryOrderedByChild("dinosaurs").observeEventType(.Value) { (s: FDataSnapshot!) -> Void in
+//            print(s)
+        }
+        
+        let some = myRootRef.childByAppendingPath("dinosaurs")
+        some.observeEventType(.Value) { (S:FDataSnapshot!) -> Void in
+//            print(S)
+        }
+        some.parent.observeEventType(.Value) { (s:FDataSnapshot!) -> Void in
+            print(s)
+        }
+        some.parent.parent.observeEventType(.Value) { (s:FDataSnapshot!) -> Void in
+            // 與上取到的層級是不一樣的喔！
+            print(s)
+        }
     }
 
     override func didReceiveMemoryWarning() {
