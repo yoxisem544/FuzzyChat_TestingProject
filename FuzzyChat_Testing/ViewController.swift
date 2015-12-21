fd04811783a1129595313987ea0b11a233742dd0
David <yoxisem544@gmail.com>
2015-12-21 11:19:15 +0800

    Initial Commit

---
+++ FuzzyChat_Testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FuzzyChat_Testing
+//
+//  Created by David on 2015/12/21.
+//  Copyright © 2015年 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
